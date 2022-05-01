53c118d7c26d0e73058598850644ac148ae45457
Cristian Novoa <cnovoa.o@gmail.com>
2022-05-01 17:47:09 +0300

    polybar delete % from volume, picom change nitrogen to feh

--- .custom-picom.sh
+++ .custom-picom.sh
@@ -2,4 +2,5 @@
 
 killall picom
 picom --experimental-backends -b &
-/usr/bin/nitrogen --random --set-zoom --set-color=[#4c566a] &
+#/usr/bin/nitrogen --random --set-zoom --set-color=[#4c566a] &
+feh -z --bg-max '/home/manjar/Documents/Images/WP/' &
