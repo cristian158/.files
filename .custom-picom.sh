5e4872719ec987a4ef57ecb6546cca65184bad66
Cristian Novoa <cnovoa.o@gmail.com>
2022-05-14 14:00:06 +0300

    readjustement

--- .custom-picom.sh
+++ .custom-picom.sh
@@ -3,4 +3,4 @@
 killall picom
 picom --experimental-backends -b &
 #/usr/bin/nitrogen --random --set-zoom --set-color=[#4c566a] &
-feh -z --bg-max '/home/manjar/Documents/Images/WP/' &
+#feh -z --bg-max '/home/manjar/Documents/Images/WP/' &
